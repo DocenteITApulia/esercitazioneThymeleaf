bf4a027699a782e792bed145f3aa485c36153ad0
AlessioCuoccio-IA <acuoccio@studenti.apuliadigitalmaker.it>
2021-09-16 12:56:23 +0200

    commit test

--- src/main/java/it/apulia/esercitazione1/Esercitazione1Application.java
+++ src/main/java/it/apulia/esercitazione1/Esercitazione1Application.java
@@ -1,6 +1,6 @@
 package it.apulia.esercitazione1;
 
-
+//commento test
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
